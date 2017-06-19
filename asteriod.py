2cf1cc357dd1bf02ab606491f4708d97e2a4898b
mwheele <mwheele1@binghamton.edu>
2017-06-19 18:45:45 -0400

    version 3
    this adds in placeholder assets for now. They can be changed later. The asteroid and bullet can now be tied to a sprite. The ship can as well, and the movement has been expanded.

--- asteriod.py
+++ asteriod.py
@@ -1,33 +1,48 @@
 import math
 import random 
+import pygame
+import os,sys
+def load_image(name, colorkey=None):
+	fullname = os.path.join("assets", name) 
+	image = pygame.image.load(fullname) 
+	image = image.convert() 
+	if colorkey is not None:
+		if colorkey is -1:
+			colorkey = image.get_at((0,0))
+	return image, image.get_rect() 
+	 
 
-class asteriod:
+	return image, image.get_rect()
+class asteriod(pygame.sprite.Sprite):
 	'''Asteriod needs some more work just a rough idea of what is needed'''
 	def __init__(self,x,y,angle):
-		self.x = x
-		self.y =y 
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('asteriod.png', -1) 
+		self.image = pygame.transform.scale(self.image, (200, 200))
+		self.rect.x = x
+		self.rect.y =y 
 		self.angle = angle 
 	def move(self):
-	'''Asteriod moves based on angle depending on the random value the asteriods will move at diffrent angles'''
+		'''Asteriod moves based on angle depending on the random value the asteriods will move at diffrent angles'''
 		print(self.angle)
 		angler = math.radians(self.angle) 
 		self.x = self.x + (math.cos(angler) * 2) 
 		self.y = self.y + (math.sin(angler) * 2)	
 		print(self.x,self.y) 
 	def angle(self):
-		x = random.randrange(0,11):
-			if x % 2 == 0:
-				self.angle += 30
-			else:
-				self.angle -=15
+		x = random.randrange(0,11)
+		if x % 2 == 0:
+			self.angle += 30
+		else:
+			self.angle -=15
 		if self.angle>=360 or self.angle<=-360:
 			self.angle = 0
 			
 	def health(self, level):
-	'''Sets the asteriods health, it will do more later'''
+		'''Sets the asteriods health, it will do more later'''
 		y = random.randrange(0,2)
-			if y == 1 :
-				self.level = 1
-			else:
-				self.level = 0 
+		if y == 1 :
+			self.level = 1
+		else:
+			self.level = 0 
 
--- bullet.py
+++ bullet.py
@@ -1,11 +1,24 @@
 import math
-class bullet:
-	def __init__(self, x, y, angle): 
-		self.x = x
-		self. y = y
+import pygame
+import os,sys
+def load_image(name, colorkey=None):
+	fullname = os.path.join("assets", name) 
+	image = pygame.image.load(fullname) 
+	image = image.convert() 
+	if colorkey is not None:
+		if colorkey is -1:
+			colorkey = image.get_at((0,0))
+	return image, image.get_rect() 
+class bullet(pygame.sprite.Sprite):
+	def __init__(self, x, y, angle):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_image('laser.png', -1)  
+		self.image = pygame.transform.scale(self.image, (50, 50))
+		self.rect.x = x
+		self.rect.y = y
 		self.angle = angle
 	def move(self):
-	'''The bullet is called then appears on the ships current x,y and with the ships current angle it then travels forward''' 
+		'''The bullet is called then appears on the ships current x,y and with the ships current angle it then travels forward''' 
 		print(self.angle)
 		angler = math.radians(self.angle) 
 		self.x = self.x + (math.cos(angler) * 10) 
