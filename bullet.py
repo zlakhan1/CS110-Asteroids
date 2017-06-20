84e1e39ce61482c7b696be1cde971d1312f2cf45
mwheele <mwheele1@binghamton.edu>
2017-06-20 15:27:19 -0400

    lab7

--- bullet.py
+++ bullet.py
@@ -1,14 +1,16 @@
 import math
-class bullet:
+import ship
+class bullet():
 	def __init__(self, x, y, angle): 
+		#ship.__init__(self,x,y,angle) 
 		self.x = x
-		self. y = y
+		self.y = y
 		self.angle = angle
 	def move(self):
-	'''The bullet is called then appears on the ships current x,y and with the ships current angle it then travels forward''' 
-		print(self.angle)
-		angler = math.radians(self.angle) 
-		self.x = self.x + (math.cos(angler) * 10) 
-		self.y = self.y + (math.sin(angler) * 10)	
+		angler = math.radians(self.angle)
+		print("angler:",angler)  
+		print("cos:",(math.cos(angler) * 10), "sin:",(math.sin(angler) * 10))  
+		self.x += (math.cos(angler) * 10) 
+		self.y += (math.sin(angler) * 10)	
 		print(self.x,self.y) 
 		
--- ship.py
+++ ship.py
@@ -1,40 +1,114 @@
-import pygame 
+#import pygame 
 import math
 import bullet
-import sys 
-def load_image():
-	fullname = os.path.join("assets", spaceship.png) 
+import os,sys 
+import asteriod 
+'''def load_image(name, colorkey=None):
+	fullname = os.path.join("assets", name) 
 	image = pygame.image.load(fullname) 
-	 
+	image = image.convert() 
+	if colorkey is not None:
+		if colorkey is -1:
+			colorkey = image.get_at((0,0))
+	return image, image.get_rect() '''
 
-	return image, image.get_rect()
-class ship(pygame.sprite.Sprite):
+class ship():#pygame.sprite.Sprite):
 	'''This is the ship object it can go forward, backward, change its angle, and shoot'''
-	def __init__(self, x , y, angle, img_file):
-		load_image(img_file) 
-		self.rect.x = x
-		self.rect.y = y
+	def __init__(self, x , y, angle):
+		#pygame.sprite.Sprite.__init__(self)
+		#self.image, self.rect = load_image('spaceshipmod.png', -1) 
+		#self. image = pygame.transform.scale(self.image, (100, 100))
+		#self.rect.x = x
+		#self.rect.y = y
+		self.x = x
+		self.y = y
 		self.angle = angle
-	def move(self,angle):
-		if direction == pygame.K_UP:
-			self.angle += 1  
-		elif direction == pygame.K_LEFT:
-			self.x -= (math.cos(self.angle) * 1) 
-			self.y -= (math.sin(self.angle) * 1)	
-		elif direction == pygame.K_DOWN:
-			self.angle -= 1
-		elif direction == pygame.K_RIGHT: 
-		  	self.x += (math.cos(self.angle) * 1) 
-			self.y += (math.sin(self.angle) * 1)
-
+		self.health = 3 
+	def move(self,direction):
+		if direction == "w":#direction[pygame.K_UP]:
+			print("up") 
+			self.angle += 10
+			if self.angle >360:
+				self.angle = 0 
+			#a = ship.rotater(self.image, 10)   
+			#self.image = a
+		elif direction == "a": #direction[pygame.K_LEFT]:
+			print(self.angle)
+			if 0<self.angle<90:
+				self.x += (math.cos(math.radians(self.angle)) * 2) 
+				self.y -= (math.sin(math.radians(self.angle)) * 2)	
+			if self.angle == 90:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)	 
+			if 90<self.angle<180:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)	
+			if self.angle == 180:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)	
+			if 180<self.angle<270:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)
+			if self.angle == 270:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)
+			if 270<self.angle<360:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)
+			if self.angle == 0 or self.angle ==360:
+				self.x += (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)
+			#if not 0<self.x<600 or not 0<self.y<600:
+			#	self.y -= 600 
+			#	self.x -= 600
+		elif direction == "s":#direction[pygame.K_DOWN]:
+			print("down") 
+			self.angle -= 10
+			if self.angle<0:
+				self.angle = 360
+			#b = ship.rotater(self.image,-10) 
+			#self.image = b
+			
+		elif direction == "d":#direction[pygame.K_RIGHT]: 
+			if 0<self.angle<90:
+				self.x -= (math.cos(math.radians(self.angle)) * 2) 
+				self.y += (math.sin(math.radians(self.angle)) * 2)	
+			if self.angle == 90:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)	 
+			if 90<self.angle<180:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)	
+			if self.angle == 180:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)	
+			if 180<self.angle<270:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)
+			if self.angle == 270:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)
+			if 270<self.angle<360:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y += (math.sin(math.radians(self.angle)) * 2)
+			if self.angle == 0 or self.angle ==360:
+				self.x -= (math.cos(math.radians(self.angle)) * 2)
+				self.y -= (math.sin(math.radians(self.angle)) * 2)
+			#if self.x>600 or self.y>600:
+	
+				
 	def shoot(self,angle):
-	'''Calls the bullet object'''
-		bill = bullet.bullet(self.x, self.y, self.angle)
-		bill.move()
+		#'''Calls the bullet object'''
+		laser = bullet.bullet(self.x, self.y, self.angle)
+		laser.move() 
+		return laser.x,laser.y,laser.angle
+	def collide (self):
+		aster = asteriod.asteriod(0,0,0)
+		if aster.x == self.rect.x or aster.y == self.rect.y:
+			self.health -= 1 
+			if self.health == 0:
+				print("game over") 
 	def update(self):
 		print("Updating") 
-def tester(key):
-	ship.move(key)
-def main():
-	key = [pygame.K_UP
-		
+
+
