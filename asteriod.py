599fbca1222b1ce6a006971dc8c1477f5d1b8fb5
mwheele <mwheele1@binghamton.edu>
2017-06-26 12:26:15 -0400

    Add files via upload

--- asteriod.py
+++ asteriod.py
@@ -3,9 +3,10 @@ import random
 import pygame
 import os,sys
 def load_image(name, colorkey=None):
+	'''Loads an image and converts it to use in pygame'''
 	fullname = os.path.join("assets", name) 
 	image = pygame.image.load(fullname) 
-	image = image.convert() 
+	image = image.convert_alpha() 
 	if colorkey is not None:
 		if colorkey is -1:
 			colorkey = image.get_at((0,0))
@@ -24,21 +25,24 @@ class asteriod(pygame.sprite.Sprite):
 		self.angle = angle 
 	def move(self):
 		'''Asteriod moves based on angle depending on the random value the asteroids will move at different angles'''
-		moverx = random.randrange(100) 
-		movery = random.randrange(100)
-		self.rect.x += float((math.cos(math.radians(self.angle)) * moverx))
-		self.rect.y -= float((math.sin(math.radians(self.angle)) * movery))
+		self.rect.x += (math.cos(math.radians(self.angle)) * 100)
+		self.rect.y -= (math.sin(math.radians(self.angle)) * 100)
+		'''Checks if the asteriod is pointed toward the first quadarant'''
 		if 0<self.angle<90:	
 			if self.rect.y<0 or self.rect.x>600:
-				self.rect.x -= float(600)
+				self.rect.x -= 600
 				if self.rect.x <0:
-					self.rect.x = float(0)
-				self.rect.y += float(600)
+					self.rect.x = 0
+				self.rect.y += 600
 				if self.rect.y >600 :
-					self.rect.y = float(600)
+					self.rect.y = 600
+
+		'''Checks if the asteroid is pointed up'''
 		if self.angle == 90:
 			if self.rect.y < 0:
 				self.rect.y = self.rect.y + 600	  
+
+		'''Checks if the asteroid is pointed toward the second quadrant'''
 		if 90<self.angle<180:	
 			if self.rect.x<0 or self.rect.y<0:
 				self.rect.x += 600
@@ -47,9 +51,12 @@ class asteriod(pygame.sprite.Sprite):
 				self.rect.y += 600
 				if self.rect.y >600 :
 					self.rect.y = 600
+
+		'''Checks if the asteriod is pointed left'''
 		if self.angle == 180:
 			if self.rect.x<0:
-				self.rect.x += 600	
+				self.rect.x += 600
+		'''Checks if the asteroid is pointed toward the third quadrant'''	
 		if 180<self.angle<270:
 			if self.rect.x<0 or self.rect.y>600:
 				self.rect.x += 600
@@ -58,9 +65,12 @@ class asteriod(pygame.sprite.Sprite):
 				self.rect.y -= 600
 				if self.rect.y<0:
 					self.rect.y = 0 
+
+		'''Checks to see of the asteroid is pointed down'''
 		if self.angle == 270:
 			if self.rect.y>600:
 				self.rect.y -= 600
+		'''Checks to see if the asteroid is pointed toward the fourth quadrant'''
 		if 270<self.angle<360:
 			if self.rect.x>600 or self.rect.y>600:
 				self.rect.x -=600
@@ -69,6 +79,7 @@ class asteriod(pygame.sprite.Sprite):
 				self.rect.y -=600
 				if self.rect.y > 600:
 					self.rect.y = 600
+		'''Checks to see if the asteroid is pointed to the right''' 
 		if self.angle == 360 or self.angle == 0:
 			if self.rect.x > 600:
 				self.rect.x -= 600
@@ -77,9 +88,14 @@ class asteriod(pygame.sprite.Sprite):
 			
 	def health(self, y ):
 		'''Sets the asteriods health'''
-		#y = random.randrange(0,2)
+		y = random.randrange(0,2)
 		if y == 1 :
 			self.level = 1
 		else:
+			'''If the asteroid is small it will change the image'''
 			self.level = 0 
 			self.image, self.rect = load_image('littleaster.png', -1) 
+	def changeHealth(self):
+		'''To be used in a collosion, will decrease the level if it is level 1 ''' 
+		if self.level == 1:
+			self.level = 0 
--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import math
 import bullet
 import os,sys 
 def load_image(name, colorkey=None):
+	'''Loads an image and converts it to use in pygame'''
 	fullname = os.path.join("assets", name) 
 	image = pygame.image.load(fullname) 
-	image = image.convert() 
+	image = image.convert_alpha() 
 	if colorkey is not None:
 		if colorkey is -1:
 			colorkey = image.get_at((0,0))
@@ -20,10 +21,10 @@ class ship(pygame.sprite.Sprite):
 		pygame.sprite.Sprite.__init__(self)
 		self.image, self.rect = load_image('spaceshipmod.png', -1) 
 		self. image = pygame.transform.scale(self.image, (100, 100))
-		self.rect.x = float(x)
-		self.rect.y = float(y)
-		self.angle = float(angle)
-		print(type(self.rect.x))
+		self.rect.x = x
+		self.rect.y = y
+		self.angle = angle
+		self.health = 3 
 	def rotater(image,angle):
 		"""rotate an image while keeping its center and size"""
 		orig_rect = image.get_rect()
@@ -33,32 +34,33 @@ class ship(pygame.sprite.Sprite):
 		rot_image = rot_image.subsurface(rot_rect).copy()
 		return rot_image
 	def move(self,direction):
-		print("move gets called") 
-		print(self.rect.x, self.rect.y)  
 		pygame.key.get_pressed
 		if direction[pygame.K_UP]:
-			print("up") 
 			self.angle += 10
-			if self.angle >360:
+			if self.angle > 360:
 				self.angle = 0 
 			a = ship.rotater(self.image, 10) 
 			self.image = a
 		elif direction[pygame.K_LEFT]:
-			print(self.angle)
-			self.rect.x += float((math.cos(math.radians(self.angle)) * 2.0))
-			self.rect.y -= float((math.sin(math.radians(self.angle)) *2.0))
-			print(type(self.rect.x))
+			self.rect.x += (math.cos(math.radians(self.angle)) * 2.0) #moves x based on cosine 
+			self.rect.y -= (math.sin(math.radians(self.angle)) *2.0) # moves y based on sine 
+			'''Checks if the ship is pointing toward first quadrant'''
 			if 0<self.angle<90:	
 				if self.rect.y<0 or self.rect.x>600:
-					self.rect.x -= float(600)
+					self.rect.x -= 600
 					if self.rect.x <0:
-						self.rect.x = float(0)
-					self.rect.y += float(600)
+						self.rect.x = 0
+					self.rect.y += 600
 					if self.rect.y >600 :
-						self.rect.y = float(600)
+						self.rect.y = 600
+
+			'''Checks if the ship is pointing toward top'''
 			if self.angle == 90:
 				if self.rect.y < 0:
-					self.rect.y = self.rect.y + 600	  
+					self.rect.y = self.rect.y + 600
+
+
+			'''Checks if the ship is pointing toward the second quadrant'''	  
 			if 90<self.angle<180:	
 				if self.rect.x<0 or self.rect.y<0:
 					self.rect.x += 600
@@ -67,9 +69,14 @@ class ship(pygame.sprite.Sprite):
 					self.rect.y += 600
 					if self.rect.y >600 :
 						self.rect.y = 600
+
+			'''Checks if the ship is pointing right'''
 			if self.angle == 180:
 				if self.rect.x<0:
 					self.rect.x += 600	
+
+
+			'''Checks if the ship is point toward the third quadrant'''
 			if 180<self.angle<270:
 				if self.rect.x<0 or self.rect.y>600:
 					self.rect.x += 600
@@ -78,9 +85,13 @@ class ship(pygame.sprite.Sprite):
 					self.rect.y -= 600
 					if self.rect.y<0:
 						self.rect.y = 0 
+			
+			'''Checks if the ship is point down'''
 			if self.angle == 270:
 				if self.rect.y>600:
 					self.rect.y -= 600
+
+			'''Checks if the ship is pointing toward the fourth quadrant'''
 			if 270<self.angle<360:
 				if self.rect.x>600 or self.rect.y>600:
 					self.rect.x -=600
@@ -89,31 +100,40 @@ class ship(pygame.sprite.Sprite):
 					self.rect.y -=600
 					if self.rect.y > 600:
 						self.rect.y = 600
+			
+			'''Checks if the ship is pointed left'''		
 			if self.angle == 360 or self.angle == 0:
 				if self.rect.x > 600:
 					self.rect.x -= 600
-
 		elif direction[pygame.K_DOWN]:
-			print("down") 
+			'''Decreases the ships angle by ten'''
 			self.angle -= 10
+			'''Converts a negative angle 360'''
 			if self.angle<0:
 				self.angle = 360
 			b = ship.rotater(self.image,-10) 
 			self.image = b
 		elif direction[pygame.K_RIGHT]: 
-			self.rect.x -= float((math.cos(math.radians(self.angle)) *2)) 
-			self.rect.y += float((math.sin(math.radians(self.angle)) *2))
+			'''Moves the ship backwards'''
+			self.rect.x -= (math.cos(math.radians(self.angle)) *2)
+			self.rect.y += (math.sin(math.radians(self.angle)) *2)
+
+			'''Checks if the ship is pointing tward the second quadrant'''
 			if 90<self.angle<180:	
 				if self.rect.y>600 or self.rect.x>600:
 					self.rect.x -= 600
 					if self.rect.x <0:
-						self.rect.x = float(0)
+						self.rect.x = 0
 					self.rect.y -= 600
 					if self.rect.y <0 :
 						self.rect.y = 0
+
+			'''Checks if the ship is pointed up'''
 			if self.angle == 90:
 				if self.rect.y > 600:
-					self.rect.y -= 600	  
+					self.rect.y -= 600	
+
+			'''Checks if the ship is pointed toward the third quadrant'''  
 			if 180<self.angle<270:	
 				if self.rect.x>600 or self.rect.y<0:
 					self.rect.x -= 600
@@ -122,9 +142,7 @@ class ship(pygame.sprite.Sprite):
 					self.rect.y += 600
 					if self.rect.y >600 :
 						self.rect.y = 600
-			if self.angle == 0 or self.angle == 360:
-				if self.rect.x<0:
-					self.rect.x += 600	
+			'''Checks if the pointed toward the first quadrant'''	
 			if 0<self.angle<90:
 				if self.rect.x<0 or self.rect.y>600:
 					self.rect.x += 600
@@ -133,9 +151,11 @@ class ship(pygame.sprite.Sprite):
 					self.rect.y -= 600
 					if self.rect.y<0:
 						self.rect.y = 0 
+			'''Checks if the ship is pointed down'''
 			if self.angle == 270:
 				if self.rect.y<0:
 					self.rect.y += 600
+			'''Checks if the ship is pointed toward the fourth quadrant'''
 			if 270<self.angle<360:
 				if self.rect.x<0 or self.rect.y<0:
 					self.rect.x +=600
@@ -144,14 +164,20 @@ class ship(pygame.sprite.Sprite):
 					self.rect.y +=600
 					if self.rect.y > 600:
 						self.rect.y = 600
+
+			'''Checks if the ship is pointed right''' 
 			if self.angle == 360 or self.angle == 0:
 				if self.rect.x > 600:
 					self.rect.x -= 600
 		elif direction[pygame.K_e]:
-			load_sound("blast.wav") 
+			'''creates and calls the bullet class to shoot'''
 			bill = bullet.bullet(self.rect.x, self.rect.y, self.angle)
 			bill.move()
-			print("bullet is :",bill.rect.x,bill.rect.y,bill.angle)
+			print(bill.rect.x, bill.rect.y, bill.angle) 
+	def livesUpdate(self):
+		'''Decreases the number of lives'''
+		self.health -= 1 
+	
 	def update(self):
 		print("Updating") 
 
