09c1f42667a96922d28c2ac1f02b25e0a1db10ef
mwheele <mwheele1@binghamton.edu>
2017-06-28 15:05:11 -0400

    Add files via upload

--- ship.py
+++ ship.py
@@ -33,7 +33,7 @@ class ship(pygame.sprite.Sprite):
 		"""rotate an image while keeping its center and size"""
 		orig_rect = image.get_rect()
 		rot_image = pygame.transform.rotate(image, angle)
-		rot_rect = orig_rect.copy()
+		rot_rect = orig_rect.copy()			#from pygame documentation comments 
 		rot_rect.center = rot_image.get_rect().center
 		rot_image = rot_image.subsurface(rot_rect).copy()
 		return rot_image
